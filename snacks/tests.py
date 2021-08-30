612765828adb6ac1710d0d93598abe5c5e1673c9
rubakanaan <rubajkanaan@gmail.com>
2021-08-30 21:21:11 +0300

    fix test

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,19 +1,33 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse , reverse_lazy
+from django.contrib.auth  import get_user_model
+from .models import Snack
 # Create your tests here.
 
 
 class SnackTests(TestCase):
+    def setUp(self):
+        self.user=get_user_model().objects.create_user(
+            username = 'aasdghj',
+            email='a@gmal.com',
+            password='1234556'
+            
+        )  
+        self.snack= Snack.objects.create(
+            name = 'a',
+            purchaser = self.user,
+            description = 'anything '
+        ) 
+
+    def test_detail_page_status_code(self):
+        url = reverse('snack_detail', args='1')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
     def test_list_page_status_code(self):
         url = reverse('snack_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-
-    def test_detail_page_status_code(self):
-        url = reverse('snack_detail')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
     def test_list_page_templete(self):
         url = reverse('snack_list')
         response = self.client.get(url)
@@ -21,7 +35,7 @@ class SnackTests(TestCase):
         self.assertTemplateUsed(response, 'base.html')
 
     def test_about_page_templete(self):
-        url = reverse('snack_detail')
+        url = reverse('snack_detail', args='1')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'snack_detail.html')
         self.assertTemplateUsed(response, 'base.html')
